059507596f4a87472e332b946d80d1928931a61a
MarauderPixie <tobias.anton.85@gmail.com>
2022-03-17 22:28:31 +0100

    let's my own thing

---
+++ src/LockedChoiceInput.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="forced_choice">
+    <div class="options">
+      <div
+        v-for="option in options"
+        :key="option"
+        class="option"
+        :class="[isActive ? 'deactivated' : 'activated']"
+        @click="onOptionClick(option); isActive = !isActive"
+      >
+        {{ option }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/**
+ * Have the participant choose between multiple options.
+ */
+export default {
+  name: 'LockedChoiceInput',
+  props: {
+    /**
+     * The possible options to choose from
+     */
+    options: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+      return {
+        isActive: false
+      };
+    },
+  methods: {
+    onOptionClick(option) {
+      /**
+       * Change event with the chosen option. Useful in combination with `response.sync`
+       */
+      this.$emit('update:response', option);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.deactivated {
+  background-color: #ffd633 !important;
+  pointer-events: none;
+}
+
+.activated {
+    /* background-color: #3333ff !important; */
+    pointer-events: auto;
+}
+
+.option {
+  background-color: #1e1e1e10;
+  border: none;
+  border-radius: 2px;
+  color: #1e1e1e;
+  cursor: pointer;
+  display: inline-block;
+  font-family: 'Lato', 'Noto Sans', sans-serif;
+  font-size: 40px;
+  line-height: 80px;
+    height: 80px;
+    width: 80px;
+  font-weight: 700;
+  letter-spacing: 0.9px;
+  margin: 0 100px;
+  outline: none;
+  padding: 5px 10px;
+  text-transform: uppercase;
+}
+
+.option:hover {
+  background-color: #1e1e1e20;
+}
+</style>
