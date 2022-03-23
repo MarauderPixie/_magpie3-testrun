bbe7f070f4e184da6b43d87c68252a1f84a00d87
MarauderPixie <tobias.anton.85@gmail.com>
2022-03-23 13:47:33 +0100

    maybe api/submit_experiment does it

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,6 +1,6 @@
 export default {
   experimentId: '16',
-  serverUrl: 'https://magpie-refactored-2.herokuapp.com/',
+  serverUrl: 'https://magpie-refactored-2.herokuapp.com/api/submit_experiment/',
   socketUrl: 'wss://magpie-refactored-2.herokuapp.com/socket',
   // this will be used in prolific mode
   // completionUrl: 'https://...',
