a1e3b53040ebbe8d5ca7a84fef27f90e3d5edfea
MarauderPixie <tobias.anton.85@gmail.com>
2022-03-23 21:07:04 +0100

    add rll condition

--- src/App.vue
+++ src/App.vue
@@ -17,23 +17,39 @@
       </button>
     </Screen>
     
-    <!-- <InstructionScreen :title="'Welcome'">
+
+    <!-- INSTRUCTIONS -->
+    <InstructionScreen :title="'Welcome'">
       Thank you for choosing deutschen Wahn.
     </InstructionScreen>
 
     <InstructionScreen :title="'Introduction'">
-      <p>In this experiment, you will be shown examples of geometric images.
-      Your job is to learn to tell whether each example belongs to the <b>Alpha</b> or <b>Beta</b> category.</p>
-      
-      <p>As you are shown each example, you will be asked to make a category judgment and then you will recieve feedback.
-      At first you will have to guess, but you will gain experience as you go along. </p>
-      
-      <p>After you have completed your training, we will ask you a series of test questions about what you have learned.</p>
-    </InstructionScreen> -->
+      <!-- no rule-related language -->
+      <div v-if="thisCond() == 1 || thisCond() == 2">
+          <p>In this experiment, you will be shown examples of geometric images.
+          Your job is to learn to tell whether each example belongs to the <b>Alpha</b> or <b>Beta</b> category.</p>
+          
+          <p>As you are shown each example, you will be asked to make a category judgment and then you will recieve feedback.
+          At first you will have to guess, but you will gain experience as you go along. </p>
+          
+          <p>After you have completed your training, we will ask you a series of test questions about what you have learned.</p>
+      </div>
+
+      <!-- rule-related language -->
+      <div v-else>
+          <p>In this experiment, you will be shown examples of geometric images.
+          Your job is to learn a rule that allows you to tell whether each example belongs to the <b>Alpha</b> or <b>Beta</b> category.</p>
+
+          <p>As you are shown each example, you will be asked to make a category judgment and then you will recieve feedback.
+          At first you will have to guess, but you will gain experience as you go along. </p>
+          
+          <p>Try your best to gain mastery of the Alpha and Beta categories.</p>
+      </div>
+    </InstructionScreen>
 
 
     <!-- TRAINING TRIALS -->
-        <Screen v-for="(trial, i) in training"
+    <Screen v-for="(trial, i) in training"
         :key="i">
             
         <img :src="trial.image" /> 
@@ -55,6 +71,21 @@
             <button @click= "$magpie.saveAndNextScreen();">Weiter</button>
         </p>
     </Screen>
+    
+
+    <InstructionScreen :title="'Introduction'">
+      <!-- no rule-related language -->
+      <div v-if="thisCond() == 1 || thisCond() == 2">
+          <p>For this part of the study, you will again choose the category you think each example belongs to. 
+          This time you will not receive feedback.</p>
+      </div>
+
+      <!-- rule-related language -->
+      <div v-else>
+          <p>For this part of the study, you will again apply the rule you learned to choose the category you 
+          think each example belongs to. This time you will not receive feedback.</p>
+      </div>
+    </InstructionScreen>
 
 
     <!-- GENERALIZATION TRIALS -->
@@ -94,24 +125,18 @@
     components: { LockedChoiceInput },
     data() {
       return {
-        training: _.shuffle(training),
-        generalization: _.shuffle(generalization)
+        training: _.shuffle(training.slice(1, 3)),
+        generalization: _.shuffle(generalization.slice(1, 3))
       };
     },
     methods: {
       thisGen: function() {
         var generation = this.$magpie.socket.generation
-
-        if (generation == 1) {
-          console.log("1st Gen!")
-        } else {
-          console.log("Next Gen!")
-        }
-
         return generation
       },
       thisCond: function() {
         var condition = this.$magpie.socket.variant
+        // console.log("This condition has rll:", condition == 3 || condition == 4)
         return condition
       }
     }
