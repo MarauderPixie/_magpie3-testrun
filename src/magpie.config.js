e54700a63fce169c813c66de979a8d6cc2d47f2d
MarauderPixie <tobias.anton.85@gmail.com>
2022-03-23 13:35:43 +0100

    undo

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,9 +1,9 @@
 export default {
   experimentId: '16',
-  serverUrl: 'https://magpie-refactored-2.herokuapp.com/experiments',
+  serverUrl: 'https://magpie-refactored-2.herokuapp.com/',
   socketUrl: 'wss://magpie-refactored-2.herokuapp.com/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://...',
+  // completionUrl: 'https://...',
   contactEmail: 'test@random.com',
   // Either 'debug', 'directLink' or 'prolific'
   mode: 'directLink',
