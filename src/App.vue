0b6f892de60934d0f501203c207130f105231bb0
MarauderPixie <tobias.anton.85@gmail.com>
2022-04-06 14:04:35 +0200

    better positioning, I guess

--- src/App.vue
+++ src/App.vue
@@ -225,8 +225,8 @@
 }
 
 @keyframes moove {
-  0%    {background-color:#5187ba; left:0px;}
-  50%   {background-color:#2696ff; left:600px; transform: rotate(300deg);}
-  100%  {background-color:#5187ba; left:0px;}
+  0%    {background-color:#5187ba; left:200px;}
+  50%   {background-color:#2696ff; left:700px; transform: rotate(300deg);}
+  100%  {background-color:#5187ba; left:200px;}
 }
 </style>
